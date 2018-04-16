3715eca8ff8467c240db33ec044f0f540f3ad021
satyamthusu123 <38396505+satyamthusu123@users.noreply.github.com>
2018-04-16 20:58:41 +0530

    Add files via upload

---
+++ SqlBulkTools.IntegrationTests/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SqlBulkTools.IntegrationTests.Model
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(13)]
+        [Index]
+        public string ISBN { get; set; }
+
+        [MaxLength(256)]
+        [Index]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public DateTime? PublishDate { get; set; }
+
+        [Required]
+        [Index]
+        public decimal? Price { get; set; }
+
+        public float? TestFloat { get; set; }
+    }
+
+}
---
+++ SqlBulkTools.IntegrationTests/Models/SchemaTest2.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SqlBulkTools.IntegrationTests.Model
+{
+    [Table("SchemaTest", Schema = "AnotherSchema")]
+    public class SchemaTest2
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        public string ColumnA { get; set; }
+    }
+}
